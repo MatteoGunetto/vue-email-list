6cecb81cb76353d0dbd627c4384c40521b3bc8ef
MatteoGunetto <Matteogunetto@tiscali.it>
2023-05-29 14:42:54 +0200

    esercizio completato

--- java.js
+++ java.js
@@ -2,3 +2,22 @@
 // generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
 // Bonus:
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            EmailsArray: []
+        }
+    },
+    mounted() {
+        for (let index = 1; index < 11; index++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((resp) => {    
+                    this.EmailsArray.push(resp.data.response);
+                })
+        }
+        }
+}).mount('#app')
